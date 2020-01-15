b39a60006a290ec054c6273a442ad57757baa30d
muscleman <musclesonvacation@hotmail.com>
2020-01-14 22:05:23 -0600

    started writing tests

--- test/turtlecoin.spec.js
+++ test/turtlecoin.spec.js
@@ -1,15 +1,49 @@
-let expect    = require('chai').expect;
+var chai = require('chai');
+var sinonChai = require("sinon-chai");
+chai.use(sinonChai);
+var expect = chai.expect;
 let assert = require('assert');
 let async = require('async');
+global.config = require('./turtle.json')
 
-global.config = require('../config_examples/turtle.json')
+
+global.log = function (severity, system, text, data) {
+    let formattedMessage = text;
+
+    if (data) {
+	data.unshift(text);
+	formattedMessage = util.format.apply(null, data);
+    }
+    console.log(`\t${formattedMessage}`)
+}
 
 let bytecoin = require('../lib/paymentprocessors/turtlecoin')
 
+let paymentProcessors = {};
 
+let callback = () => {}
 
-describe('Turtlecoin Payment Processor', function() {
-	it('should', function() {
-		expect(1).to.equal(1)
+describe('Turtlecoin Payment Processor', () => {
+
+    before(() => {
+	paymentProcessors = {}
+        bytecoin(paymentProcessors)
+    })
+
+    describe('Initialize processPayments', () => {
+	it('Should contain function', () => {
+            expect(typeof(paymentProcessors['turtlecoin'])).to.equal('function')
 	})
+    })
+
+    describe('Handle processPayments', () =>{
+	it("Should display 'No workers\' balances reached the minium payment threshold", () => {
+	    let payments = {}
+	    let minPayoutLevel = {}
+	    // spy on global.log
+	    paymentProcessors['turtlecoin'](payments, minPayoutLevel, callback)
+	    // expect global.log to havebeeencalledwith("Should display 'No workers\' balances reached the minium payment threshold")
+	    expect(1).to.equal(1)
+	})
+    })
 })
