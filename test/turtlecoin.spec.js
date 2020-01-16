a68c4ef9ea70a7f4ae9bc63441aa70a4aa802e7d
muscleman <musclesonvacation@hotmail.com>
2020-01-15 19:03:46 -0600

    altered apiInterfaces

--- test/turtlecoin.spec.js
+++ test/turtlecoin.spec.js
@@ -7,27 +7,16 @@ let assert = require('assert');
 let async = require('async');
 global.config = require('./turtle.json')
 
-
 global.log = function (severity, system, text, data) {}
-/*    let formattedMessage = text;
-
-    if (data) {
-	data.unshift(text);
-	formattedMessage = util.format.apply(null, data);
-    }
-    console.log(`\t${formattedMessage}`)
-}
-*/
 
 //let utils = require('../lib/utils.js');
 
 let bytecoin = require('../lib/paymentprocessors/turtlecoin')
-let apiInterfaces = require('../lib/apiInterfaces.js')(config.daemon, config.wallet, config.api);
+let apiInterfaces = require('../lib/apiInterfaces.js')
 
-let paymentProcessors = {};
+let paymentProcessors = {}
 
 let callback = (result) => {return result}
-
 
 describe('Turtlecoin Payment Processor', () => {
 
@@ -44,24 +33,33 @@ describe('Turtlecoin Payment Processor', () => {
 
     describe('No Payments', () =>{
 	it("Should display log message 'No workers\' balances reached the minimum payment threshold", () => {
+	    //ARRANGE
 	    let balances = {}
 	    let minPayoutLevel = {}
 	    sinon.stub(global, 'log')
+	
+	    //ACT
 	    paymentProcessors['turtlecoin'](balances, minPayoutLevel, callback)
+
+	    //ASSERT
 	    expect(global.log).to.have.been.calledWith("info", "payments - turtlecoin", "No workers\' balances reached the minimum payment threshold")
 	})
     })
 
-    describe('Payments', () =>{
+    describe('Prepare payments', () =>{
         it("Should send correct parameters to 'apiInterfaces.jsonHttpRequest'", () => {
+	    //ARRANGE
 	    let balances = {  "aRi1cDd6LkAcc1p6W58dkPi8xSfbZ5EuYFrHxwH3py1MQ9rFrzmSaghguD4GGpCfHSMmKXWJrd4e5CkabC3viWJKfHuDLYqHNGs9D83sj6BPX": 234895806015}
             let minPayoutLevel = {  "aRi1cDd6LkAcc1p6W58dkPi8xSfbZ5EuYFrHxwH3py1MQ9rFrzmSaghguD4GGpCfHSMmKXWJrd4e5CkabC3viWJKfHuDLYqHNGs9D83sj6BPX":1000000000}
-	    let data = ''
-	    let endpoint = '/transactions/send/prepared'
-            sinon.stub(apiInterfaces, 'jsonHttpRequest')
+	    data = JSON.stringify({"destinations":[{"amount":100000000,"address":"aRi1cDd6LkAcc1p6W58dkPi8xSfbZ5EuYFrHxwH3py1MQ9rFrzmSaghguD4GGpCfHSMmKXWJrd4e5CkabC3viWJKfHuDLYqHNGs9D83sj6BPX"}],"feePerByte":50,"unlock_time":0})
+	    let endpoint = '/transactions/prepare/advanced'
+            sinon.stub(apiInterfaces, 'jsonHttpRequest').callsFake(function(host, port, data, callback, path) {return callback(null, {})})
+
+	    //ACT
             paymentProcessors['turtlecoin'](balances, minPayoutLevel, callback)
-	    expect(apiInterfaces.jsonHttpRequest.calledOnce).to.be.true
-//            expect(apiInterfaces.jsonHttpRequest).to.have.been.calledWith(config.wallet.host, config.wallet.port, data, null, endpoint)
+
+	    //ASSERT
+            expect(apiInterfaces.jsonHttpRequest).to.have.been.calledWith(config.wallet.host, config.wallet.port, data, sinon.match.any, endpoint)
         })
     })
 })
